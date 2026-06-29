396f2fd7b59af492622175c94e4cec9ed3d89ee7
kevindaniels2004 <karanbisoyie2004@gmail.com>
2026-06-29 07:51:55 +0530

    Install Tailwind Vite plugin

--- gym_pod/client/src/pages/Membership.tsx
+++ gym_pod/client/src/pages/Membership.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
